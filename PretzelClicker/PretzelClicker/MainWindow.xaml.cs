0d0286882e9e2f2c1684d9cd8f2dd3377e08bbdf
Alexander Schneider <Alexander.Schneider1996@hotmail.com>
2019-02-19 18:32:52 +0100

    Add comments

--- PretzelClicker/PretzelClicker/MainWindow.xaml.cs
+++ PretzelClicker/PretzelClicker/MainWindow.xaml.cs
@@ -21,9 +21,10 @@ namespace PretzelClicker
     /// </summary>
     public partial class MainWindow : Window
     {
-        SoundPlayer player = new SoundPlayer("Background_music.wav");
+        SoundPlayer player = new SoundPlayer("Background_music.wav");//Background music
 
         public int Pretzel = 0;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -33,15 +34,14 @@ namespace PretzelClicker
         {
             HideMainMenu();
             ShowGame();
-        }
+        }//end BtnPlay_Click
 
         private void HideMainMenu()
         {
             btnExit.Visibility = Visibility.Hidden;
             btnOptions.Visibility = Visibility.Hidden;
             btnPlay.Visibility = Visibility.Hidden;
-
-        }
+        }//end HideMainMenu
 
         private void ShowMainMenu()
         {
@@ -49,44 +49,43 @@ namespace PretzelClicker
             btnOptions.Visibility = Visibility.Visible;
             btnPlay.Visibility = Visibility.Visible;
 
-        }
-        
+        }//end ShowMainMenu
+
         private void ShowGame()
         {
             btnClickField.Visibility = Visibility.Visible;
             lblPretzel.Visibility = Visibility.Visible;
-        }
+        }//end ShowGame
 
         private void HideGame()
         {
             btnClickField.Visibility = Visibility.Hidden;
             lblPretzel.Visibility = Visibility.Hidden;
-        }
+        }//end HideGame
 
         private void UpdateScore()
         {
             lblPretzel.Content = "Pretzel: " + Pretzel;
-        }
+        }//end UpdateScore
 
         private void BtnClickField_Click(object sender, RoutedEventArgs e)
         {
             Pretzel++;
             UpdateScore();
-        }
+        }//end BtnClickField_Click
 
         private void WinMain_Loaded(object sender, RoutedEventArgs e)
         {
             ShowMainMenu();
             HideGame();
-            player.PlayLooping();
-            //player.Play();
-        }
+            player.PlayLooping(); //Loop background_music
+        }//end WinMain_Loaded
 
         private void BtnGrandma_Click(object sender, RoutedEventArgs e)
         {
             Pretzel = Pretzel + 2;
             UpdateScore();
-        }
+        }//end BtnGrandma_Click
 
         private void LoadSettings()
         {
@@ -96,6 +95,7 @@ namespace PretzelClicker
         private void CloseGame()
         {
             //Properties.Settings.Default.Pretzel = Pretzel;
+            //Application.Exit();
         }
 
         private void WinMain_Closed(object sender, EventArgs e)
